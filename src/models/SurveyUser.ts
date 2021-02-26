bc2d11a0f327f2f6e72ea2be786159a74aea50c9
Thiago Rocha <mario.thiago247@gmail.com>
2021-02-26 19:39:04 -0300

    link config

---
+++ src/models/SurveyUser.ts
@@ -0,0 +1,38 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+import { Survey } from "./Survey";
+import { User } from "./user";
+
+@Entity("surveys_users")
+class SurveyUser {
+    @PrimaryColumn()
+    readonly id: string;
+    
+    @Column()
+    user_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({name: "user_id"})
+    user: User;
+
+    @ManyToOne(() => Survey)
+    @JoinColumn({name: "survey_id"})
+    survey: Survey;
+
+    @Column()
+    value: number;
+
+    @Column()
+    survey_id: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid()
+        }
+    }
+}
+
+export { SurveyUser }
